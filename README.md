2de8f406fa4ac53e989e3863a078b5c1d8906bfa
Robin Edmunds <r.edmunds@live.co.uk>
2020-11-13 16:17:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chocolatey Managed Windows Software
+
+Running `choco-install-all.ps1` in PowerShell will install all software packages in the list from the chocolatey community package index. All these software packages are then managed by the Chocolatey package manager for easy updating.
+
+## First run
+
+1. [Install chocolatey](https://chocolatey.org/install)
+1. Open elevated PowerShell shell (Ctrl-Shift click)
+1. Run `./choco-install-all.ps1`
+
+## Updating packages
+
+1. Open elevated PowerShell shell (Ctrl-Shift click)
+1. Run `choco outdated` to get list of outdated packages
+1. Run `choco upgrade all --yes` to upgrade all outdated packages
+
+[Chocolatey command reference](https://chocolatey.org/docs/commands-reference)
---
+++ choco-install-all.ps1
@@ -0,0 +1,80 @@
+# ===========================================
+# ROBIN'S CHOCOLATEY MANAGED WINDOWS SOFTWARE
+# ===========================================
+
+# ---------
+# UNMANAGED
+# ---------
+
+# adobe xd
+# album art downloader
+# all drivers
+# authy (2fa)
+# battle.net
+# chrome
+# discord
+# edge
+# figma
+# firefox
+# free download manager 3
+# origin
+# simpledlna
+# spotify
+# steam
+# steelseries engine (mouse)
+# teamspeak
+# telegram
+# thunderbird
+# typora (markdown)
+# uplay (ubisoft)
+# vs code
+
+
+# -------
+# MANAGED
+# -------
+
+choco install --yes `
+  7zip `
+  adobereader `
+  audacity `
+  blender `
+  bulkrenameutility `
+  ccleaner `
+  cdburnerxp `
+  cpu-z `
+  curl `
+  everything `
+  eac <# exact audio copy #> `
+  f.lux `
+  ffmpeg `
+  filezilla `
+  foobar2000 `
+  fsviewer `
+  gimp `
+  git `
+  gpg4win `
+  handbrake `
+  hexchat `
+  hwinfo `
+  inkscape `
+  keepass `
+  libreoffice-fresh `
+  microsoft-windows-terminal `
+  mpv `
+  nextcloud-client `
+  nmap `
+  nodejs `
+  notepadplusplus.install `
+  obs-studio `
+  postman `
+  powershell-core `
+  python `
+  rufus `
+  sharex `
+  sqlitebrowser `
+  streamlink `
+  treesizefree `
+  ultravnc `
+  vlc `
+  vmware-workstation-player
