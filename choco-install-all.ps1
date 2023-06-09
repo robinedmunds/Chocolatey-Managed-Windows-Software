5244901776e8f7d09e2fb2a7c6e96929bb5bdd3f
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-06-09 13:49:24 +0100

    Add file .editorconfig. Fix comments

--- choco-install-all.ps1
+++ choco-install-all.ps1
@@ -10,14 +10,17 @@
 # album art downloader
 # all drivers
 # authy (2fa)
-# battle.net
+# battle.net (blizzard)
+# brave
 # chrome
+# davinci resolve (video editing)
 # discord
-# edge
+# ea games
 # figma
 # firefox
 # free download manager 3
-# origin
+# microsoft edge
+# microsoft office
 # simpledlna
 # spotify
 # steam
@@ -34,53 +37,53 @@
 # -------
 
 choco install --yes `
-  7zip `
-  adobereader `
-  audacity `
-  autohotkey `
-  blender `
-  bulkrenameutility `
-  ccleaner `
-  cdburnerxp `
-  cpu-z `
-  curl `
-  eac <# exact audio copy #> `
-  everything `
-  f.lux `
-  ffmpeg `
-  filezilla `
-  flac `
-  foobar2000 `
-  fsviewer `
-  gimp `
-  git `
-  gpg4win `
-  handbrake `
-  hexchat `
-  hwinfo `
-  inkscape `
-  keepass `
-  libreoffice-fresh `
-  microsoft-windows-terminal `
-  mpv `
-  neovim `
-  nextcloud-client `
-  nmap `
-  nodejs-lts `
-  notepadplusplus.install `
-  obs-studio `
-  postman `
-  powershell-core `
-  powertoys `
-  python `
-  restic `
-  rufus `
-  sharex `
-  sqlitebrowser `
-  streamlink `
-  treesizefree `
-  typora <# LICENSED #> `
-  ultravnc `
-  vlc `
-  vmware-workstation-player `
-  yt-dlp
+    7zip `
+    adobereader `
+    audacity `
+    autohotkey `
+    blender `
+    bulkrenameutility `
+    ccleaner `
+    cdburnerxp `
+    cpu-z `
+    curl `
+    eac <# exact audio copy #> `
+    everything `
+    f.lux `
+    ffmpeg `
+    filezilla `
+    flac `
+    foobar2000 `
+    fsviewer `
+    gimp `
+    git `
+    gpg4win `
+    handbrake `
+    hexchat `
+    hwinfo `
+    inkscape `
+    keepass `
+    libreoffice-fresh `
+    microsoft-windows-terminal `
+    mpv `
+    neovim `
+    nextcloud-client `
+    nmap `
+    nodejs-lts `
+    notepadplusplus.install `
+    obs-studio `
+    postman `
+    powershell-core `
+    powertoys `
+    python `
+    restic `
+    rufus `
+    sharex `
+    sqlitebrowser `
+    streamlink `
+    treesizefree `
+    typora <# LICENSED #> `
+    ultravnc `
+    vlc `
+    vmware-workstation-player `
+    yt-dlp
