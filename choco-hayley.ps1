b11fcd950ba62eba2eaa571d4a11e5f08ee13bf1
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-10-12 15:15:20 +0100

    Add file choco-hayley.ps1

---
+++ choco-hayley.ps1
@@ -0,0 +1,52 @@
+# ===========================================
+# HAYLEY'S CHOCOLATEY MANAGED WINDOWS SOFTWARE
+# ===========================================
+
+# ---------
+# UNMANAGED
+# ---------
+
+# all drivers
+# authy (2fa)
+# brave
+# chrome
+# ea games
+# steam
+# uplay (ubisoft)
+
+
+# -------
+# MANAGED
+# -------
+
+choco install --yes `
+    7zip `
+    adobereader `
+    cpu-z `
+    curl `
+    discord `
+    everything `
+    ffmpeg `
+    filezilla `
+    firefox `
+    foobar2000 `
+    fsviewer `
+    gimp `
+    hwinfo `
+    inkscape `
+    keepass `
+    libreoffice-fresh `
+    mpv `
+    nmap `
+    notepadplusplus.install `
+    python `
+    restic `
+    rufus `
+    sharex `
+    spotify `
+    streamlink `
+    teamspeak `
+    telegram `
+    treesizefree `
+    vlc `
+    yt-dlp
